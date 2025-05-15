6dda00bda459821bf70ebd275dfdeb821ca8d2f0
Saroath <saroathj@gmail.com>
2025-05-16 01:40:30 +0530

    Simple Pizza Bill Generator System Created

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        //Pizza basePizza = new Pizza(false);
+       // basePizza.addExtraCheese();
+        //basePizza.addExtraToppings();
+       // basePizza.takeAway();
+        // basePizza.getBill();
+
+        DeluxPizza dp = new DeluxPizza(true);
+        dp.addExtraToppings();
+        dp.addExtraCheese();
+        dp.getBill();
+
+    }
+}
